5adc518831677c47475955aaabc683f084f444e1
Stuart Green <github.47a8z@aleeas.com>
2022-01-13 22:39:58 +0000

    even or odd

---
+++ 8-kyu/even-or-odd.js
@@ -0,0 +1,8 @@
+/* 
+    Create a function that takes an integer as an argument and
+    returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+const evenOrOdd = () => {
+    return (number % 2 === 0) ? 'Even' : 'Odd';
+};
