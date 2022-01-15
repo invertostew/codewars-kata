6b24d75d1f62f7dcedb38d35565eaf9abc7c683a
Stuart Green <github.47a8z@aleeas.com>
2022-01-15 22:26:38 +0000

    char from ascii

---
+++ 8-kyu/get-character-from-ascii-value.js
@@ -0,0 +1,15 @@
+/* 
+    Write a function get_char() / getChar() which takes
+    a number and returns the corresponding ASCII char
+    for that value
+*/
+
+const getChar = (c) => {
+    return String.fromCharCode(c);
+};
+
+/* 
+    Implicit return version:
+    
+    const getChar = c => String.fromCharCode(c);
+*/
