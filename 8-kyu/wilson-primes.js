28026ec26e02cb6f1b4903a6807106aea3054766
Stuart Green <github.47a8z@aleeas.com>
2022-01-18 22:54:25 +0000

    wilson primes

---
+++ 8-kyu/wilson-primes.js
@@ -0,0 +1,13 @@
+/* 
+    Wilson primes satisfy the following condition. Let P represent a prime number.
+
+    Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+    Your task is to create a function that returns true if the given number is a Wilson prime.
+*/
+
+const amIWilson = p => {
+    const factorial = x => x <= 1 ? 1 : x * factorial(x - 1);
+
+    return (factorial(p - 1) + 1) / (p * p) % 1 === 0;
+};
